de896cbd17073fec4742d4869f8fc4eda666da13
Nikita_Vasilkov <vasilkov.nikitcka@yandex.ru>
2025-12-12 17:53:42 +0300

    PalindromeLine удаление прабелов перевод в нижний регистр и поиск палиндромов

---
+++ src/Practicum/Sprint_7/String/QuestString/StringBuilder/Quest/Palindrome.java
@@ -0,0 +1,11 @@
+package Practicum.Sprint_7.String.QuestString.StringBuilder.Quest;
+
+public class Palindrome {
+    public boolean isPalindromeWord(String str) {
+        if (str == null){
+            return false;
+        }
+        String reverse = new StringBuilder(str).reverse().toString();
+        return reverse.equals(str);
+    }
+}
---
+++ src/Practicum/Sprint_7/String/QuestString/StringBuilder/Quest/PalindromeLine.java
@@ -0,0 +1,27 @@
+package Practicum.Sprint_7.String.QuestString.StringBuilder.Quest;
+
+public class PalindromeLine {
+    public boolean isPalindromeLine(String str) {
+
+        // 1. Приводим к нижнему регистру и создаем StringBuilder
+        StringBuilder original = new StringBuilder(str.toLowerCase());
+
+        // 2. Удаляем пробелы циклом
+        int i = 0;
+        while (i < original.length()) {
+            if (original.charAt(i) == ' ') {
+                original.deleteCharAt(i);
+                // Не увеличиваем i, так как следующий символ сместился на позицию i
+            } else {
+                i++;  // Переходим к следующему символу
+            }
+        }
+
+        // 3. Создаем перевернутую копию
+        StringBuilder reversed = new StringBuilder(original);
+        reversed.reverse();
+
+        // 4. Сравниваем
+        return original.toString().equals(reversed.toString());
+    }
+}
